a478cbc1af8d28e920aea65c081a6492ee8f70e8
Jon Penwood <jonp@polarity.io>
2020-10-29 09:46:53 -0500

    Updated logging level and rejectUnauthorized property in config.js

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
    * @type String
    * @required
    */
-  name: "TenableSC",
+  name: 'TenableSC',
   /**
    * The acronym that appears in the notification window when information from this integration
    * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
@@ -15,7 +15,7 @@ module.exports = {
    * @type String
    * @required
    */
-  acronym: "SC",
+  acronym: 'SC',
   /**
    * Description for this integration which is displayed in the Polarity integrations user interface
    *
@@ -23,7 +23,7 @@ module.exports = {
    * @optional
    */
   description:
-    "With Tenable.sc (formerly SecurityCenter) you get a real-time, continuous assessment of your security posture so you can find and fix vulnerabilities faster.",
+    'With Tenable.sc (formerly SecurityCenter) you get a real-time, continuous assessment of your security posture so you can find and fix vulnerabilities faster.',
   entityTypes: ['IPv4', 'domain'],
   /**
    * An array of style files (css or less) that will be included for your integration. Any styles specified in
@@ -32,7 +32,7 @@ module.exports = {
    * @type Array
    * @optional
    */
-  styles: ["./styles/sc.less"],
+  styles: ['./styles/sc.less'],
   /**
    * Provide custom component logic and template for rendering the integration details block.  If you do not
    * provide a custom template and/or component then the integration will display data as a table of key value
@@ -43,41 +43,41 @@ module.exports = {
    */
   block: {
     component: {
-      file: "./components/sc-block.js"
+      file: './components/sc-block.js'
     },
     template: {
-      file: "./templates/sc-block.hbs"
+      file: './templates/sc-block.hbs'
     }
   },
   summary: {
     component: {
-      file: "./components/sc-summary.js"
+      file: './components/sc-summary.js'
     },
     template: {
-      file: "./templates/sc-summary.hbs"
+      file: './templates/sc-summary.hbs'
     }
   },
   request: {
     // Provide the path to your certFile. Leave an empty string to ignore this option.
     // Relative paths are relative to the Urlhaus integration's root directory
-    cert: "",
+    cert: '',
     // Provide the path to your private key. Leave an empty string to ignore this option.
     // Relative paths are relative to the Urlhaus integration's root directory
-    key: "",
+    key: '',
     // Provide the key passphrase if required.  Leave an empty string to ignore this option.
     // Relative paths are relative to the Urlhaus integration's root directory
-    passphrase: "",
+    passphrase: '',
     // Provide the Certificate Authority. Leave an empty string to ignore this option.
     // Relative paths are relative to the Urlhaus integration's root directory
-    ca: "",
+    ca: '',
     // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
     // the url parameter (by embedding the auth info in the uri)
-    proxy: "",
+    proxy: '',
 
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   },
   logging: {
-    level: "trace" //trace, debug, info, warn, error, fatal
+    level: 'info' //trace, debug, info, warn, error, fatal
   },
   /**
    * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
@@ -88,30 +88,29 @@ module.exports = {
    */
   options: [
     {
-      key: "url",
-      name: "Base tenable.sc API URL",
-      description:
-        "The base URL for the tenable.sc API including the schema (i.e., https://)",
-      type: "text",
-      default: "",
+      key: 'url',
+      name: 'Base tenable.sc API URL',
+      description: 'The base URL for the tenable.sc API including the schema (i.e., https://)',
+      type: 'text',
+      default: '',
       userCanEdit: false,
       adminOnly: true
     },
     {
-      key: "userName",
-      name: "Valid Username",
-      description: "Valid tenable.sc Username",
-      default: "",
-      type: "text",
+      key: 'userName',
+      name: 'Valid Username',
+      description: 'Valid tenable.sc Username',
+      default: '',
+      type: 'text',
       userCanEdit: true,
       adminOnly: false
     },
     {
-      key: "password",
-      name: "Valid Password",
-      description: "Valid tenable.sc Password",
-      default: "",
-      type: "password",
+      key: 'password',
+      name: 'Valid Password',
+      description: 'Valid tenable.sc Password',
+      default: '',
+      type: 'password',
       userCanEdit: true,
       adminOnly: false
     }
