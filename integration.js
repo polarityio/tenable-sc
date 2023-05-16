f0796161dd1915ff5a1502f26d4cd5d41ae067d1
Jon Penwood <jonp@polarity.io>
2023-05-16 15:29:02 -0500

    fix: Fixed the limiter code and made compatible with develop

--- integration.js
+++ integration.js
@@ -1,24 +1,38 @@
 'use strict';
 
+const Bottleneck = require('bottleneck/es5');
 const request = require('postman-request');
 const _ = require('lodash');
 const fp = require('lodash/fp');
 const config = require('./config/config');
 const { version: packageVersion } = require('./package.json');
-const async = require('async');
 const fs = require('fs');
 const { DateTime } = require('luxon');
 
 let Logger;
 let requestWithDefaults;
+let ipBlocklistRegex = null;
 
-const MAX_PARALLEL_LOOKUPS = 10;
+const MAX_ENTITY_LENGTH = 100;
+const IGNORED_IPS = new Set(['127.0.0.1', '255.255.255.255', '0.0.0.0']);
+
+let limiter = null;
+
 const USER_AGENT = `polarity-tenablesc-integration-v${packageVersion}`;
 
 const NodeCache = require('node-cache');
 const tokenCache = new NodeCache({
   stdTTL: 1000 * 1000
 });
+
+const _setupLimiter = (options) => {
+  limiter = new Bottleneck({
+    maxConcurrent: fp.parseInt(10, options.maxConcurrent), // no more than options.maxConcurrent lookups can be running at single time
+    highWater: 100, // no more than 100 lookups can be queued up
+    strategy: Bottleneck.strategy.OVERFLOW,
+    minTime: fp.parseInt(10, options.minTime) // don't run lookups faster than 1 every options.minTime ms
+  });
+};
 
 /**
  *
@@ -64,7 +78,8 @@ function startup(logger) {
 }
 
 const getTokenCacheKey = (options) => options.apiKey + options.apiSecret;
-const statusCodeIsInvalid = (statusCode) => [200, 404, 202].every((validStatusCode) => statusCode !== validStatusCode);
+const statusCodeIsInvalid = (statusCode) =>
+  [200, 404, 202].every((validStatusCode) => statusCode !== validStatusCode);
 
 function getAuthToken({ url: tenableScUrl, userName, password, ...options }, callback) {
   let cacheKey = getTokenCacheKey(options);
@@ -77,8 +92,7 @@ function getAuthToken({ url: tenableScUrl, userName, password, ...options }, cal
         username: userName,
         password
       },
-      json: true,
-      timeout: 30000
+      json: true
     },
     (err, resp, body) => {
       if (err) {
@@ -105,7 +119,11 @@ function getAuthToken({ url: tenableScUrl, userName, password, ...options }, cal
       let cookie = resp.headers['set-cookie'][1];
 
       if (typeof cookie === undefined) {
-        callback({ detail: `Response did not include expected cookie`, body, statusCode: resp.statusCode });
+        callback({
+          detail: `Response did not include expected cookie`,
+          body,
+          statusCode: resp.statusCode
+        });
         return;
       }
 
@@ -119,128 +137,204 @@ function getAuthToken({ url: tenableScUrl, userName, password, ...options }, cal
 }
 
 function doLookup(entities, options, cb) {
-  let lookupResults = [];
-  let tasks = [];
+  try {
+    const lookupResults = [];
+    const errors = [];
+    const blockedEntities = [];
+    let numConnectionResets = 0;
+    let numThrottled = 0;
+    let hasValidIndicator = false;
 
-  Logger.debug(entities);
+    if (!limiter) _setupLimiter(options);
 
-  getAuthToken(options, (err, token) => {
-    if (err) {
-      Logger.error(err, 'Error getting auth token');
-      cb({
-        detail: err.detail ? err.detail : 'Unable to authenticate to Tenable',
-        err
+    Logger.debug(entities);
+
+    getAuthToken(options, (err, token) => {
+      try {
+        if (err) {
+          Logger.error(err, 'Error getting auth token');
+          cb({
+            detail: err.detail ? err.detail : 'Unable to authenticate to Tenable',
+            err
+          });
+          return;
+        }
+
+        Logger.trace({ token }, 'Retrieved Token');
+
+        let { cookie } = token;
+        let cookieJar = request.jar();
+        cookieJar.setCookieSync(cookie, options.url);
+
+        Logger.trace({ token }, 'what does the token look like in doLookup');
+
+        entities.forEach((entity) => {
+          if (!_isInvalidEntity(entity) && !_isEntityBlocklisted(entity, options)) {
+            hasValidIndicator = true;
+
+            limiter.submit(
+              _fetchApiData,
+              entity,
+              token,
+              cookieJar,
+              options,
+              (err, result) => {
+                const maxRequestQueueLimitHit =
+                  (_.isEmpty(err) && _.isEmpty(result)) ||
+                  (err && err.message === 'This job has been dropped by Bottleneck');
+                const statusCode = _.get(err, 'statusCode', '');
+                const isGatewayTimeout = statusCode === 502 || statusCode === 504;
+                const isConnectionReset = _.get(err, 'error.code', '') === 'ECONNRESET';
+
+                if (maxRequestQueueLimitHit || isConnectionReset || isGatewayTimeout) {
+                  // Tracking for logging purposes
+                  if (isConnectionReset) numConnectionResets++;
+                  if (maxRequestQueueLimitHit) numThrottled++;
+
+                  lookupResults.push({
+                    entity,
+                    isVolatile: true,
+                    data: {
+                      summary: ['! Lookup limit reached'],
+                      details: {
+                        maxRequestQueueLimitHit,
+                        isConnectionReset,
+                        isGatewayTimeout,
+                        summaryTag: '! Lookup limit reached',
+                        errorMessage:
+                          'The search failed due to the API search limit. You can retry your search by pressing the "Retry Search" button.'
+                      }
+                    }
+                  });
+                } else if (err) {
+                  errors.push(err);
+                } else {
+                  const body = fp.get('body', result);
+
+                  if (
+                    body === null ||
+                    _isMiss(body) ||
+                    _.isEmpty(body) ||
+                    (body.response.results && _.isEmpty(body.response.results)) ||
+                    (body.response.repositories && _.isEmpty(body.response.repositories))
+                  ) {
+                    lookupResults.push({
+                      entity,
+                      data: null
+                    });
+                  } else {
+                    const formattedDetails = getFormattedDetails(body, options, entity);
+                    lookupResults.push({
+                      entity,
+                      data: {
+                        summary: getSummaryTags(formattedDetails),
+                        details: formattedDetails
+                      }
+                    });
+                  }
+                }
+
+                if (
+                  lookupResults.length + errors.length + blockedEntities.length ===
+                  entities.length
+                ) {
+                  if (numConnectionResets > 0 || numThrottled > 0) {
+                    Logger.warn(
+                      {
+                        numEntitiesLookedUp: entities.length,
+                        numConnectionResets: numConnectionResets,
+                        numLookupsThrottled: numThrottled
+                      },
+                      'Lookup Limit Error'
+                    );
+                  }
+                  // we got all our results
+                  if (errors.length > 0) {
+                    cb(errors);
+                  } else {
+                    Logger.trace({ LOOK: lookupResults });
+                    cb(null, lookupResults);
+                  }
+                }
+              }
+            );
+          } else {
+            blockedEntities.push(entity);
+          }
+        });
+        if (!hasValidIndicator) {
+          cb(null, []);
+        }
+      } catch (error) {
+        const parseErrorToReadableJson = (error) =>
+          JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+        Logger.trace({ error: parseErrorToReadableJson(error) }, 'Error in doLookup');
+      }
+    });
+  } catch (error) {
+    const parseErrorToReadableJson = (error) =>
+      JSON.parse(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+    Logger.trace({ error: parseErrorToReadableJson(error) }, 'Error in doLookup');
+  }
+}
+
+const _fetchApiData = (entity, token, cookieJar, options, cb) => {
+  // building request options...
+  let requestOptions = {
+    headers: {
+      'X-SecurityCenter': token.token
+    },
+    jar: cookieJar,
+    json: true
+  };
+
+  Logger.trace({ REQUEST_OPTIONS: requestOptions });
+  if (entity.isIPv4) {
+    (requestOptions.method = 'GET'),
+      (requestOptions.uri = `${options.url}/rest/deviceInfo`),
+      (requestOptions.qs = {
+        ip: `${entity.value}`
       });
-      return;
+  } else if (entity.type === 'cve') {
+    (requestOptions.method = 'POST'),
+      (requestOptions.uri = `${options.url}/rest/analysis`),
+      (requestOptions.body = {
+        query: {
+          type: 'vuln',
+          tool: 'listvuln',
+          startOffset: 0,
+          endOffset: options.maxResults || 50,
+          filters: [{ filterName: 'cveID', operator: '=', value: entity.value }],
+          vulnTool: 'listvuln'
+        },
+        sourceType: 'cumulative',
+        type: 'vuln'
+      });
+  } else {
+    return;
+  }
+  // MAKE REQUESTS FOR ENTITY DATA...
+  requestWithDefaults(requestOptions, function (error, res, body) {
+    const statusCode = res && res.statusCode;
+
+    if (error) {
+      return cb(error);
     }
 
-    Logger.trace({ token }, 'Retrieved Token');
+    Logger.trace(requestOptions);
+    Logger.trace({ body, statusCode: statusCode || 'N/A' }, 'Result of Lookup');
 
-    let { cookie } = token;
-    let cookieJar = request.jar();
-    cookieJar.setCookieSync(cookie, options.url);
-
-    entities.forEach(async (entity) => {
-      const requestOptions = {
-        headers: {
-          'X-SecurityCenter': token.token
-        },
-        jar: cookieJar,
-        json: true
-      };
-
-      if (entity.isIPv4) {
-        (requestOptions.method = 'GET'),
-          (requestOptions.uri = `${options.url}/rest/deviceInfo`),
-          (requestOptions.qs = {
-            ip: `${entity.value}`
-          });
-      } else if (entity.type === 'cve') {
-        (requestOptions.method = 'POST'),
-          (requestOptions.uri = `${options.url}/rest/analysis`),
-          (requestOptions.body = {
-            query: {
-              type: 'vuln',
-              tool: 'listvuln',
-              startOffset: 0,
-              endOffset: options.maxResults || 50,
-              filters: [{ filterName: 'cveID', operator: '=', value: entity.value }],
-              vulnTool: 'listvuln'
-            },
-            sourceType: 'cumulative',
-            type: 'vuln'
-          });
-      } else {
-        return;
-      }
-
-      Logger.trace({ uri: requestOptions }, 'Request URI');
-
-      tasks.push(function (done) {
-        requestWithDefaults(requestOptions, function (error, res, body) {
-          const statusCode = res && res.statusCode;
-          if (error) {
-            return done({
-              detail: 'HTTP Network error',
-              error
-            });
-          }
-
-          Logger.trace(requestOptions);
-          Logger.trace({ body, statusCode: statusCode || 'N/A' }, 'Result of Lookup');
-
-          if (statusCodeIsInvalid(statusCode)) {
-            return done({
-              body,
-              detail: body && body.error_msg ? body.error_msg : 'Unexpected error encountered',
-              statusCode
-            });
-          }
-
-          done(null, {
-            entity,
-            body: statusCode === 200 ? body : null
-          });
-        });
+    if (statusCodeIsInvalid(statusCode))
+      cb({
+        err: body,
+        detail: `${body.error}: ${body.message}`
       });
-    });
-
-    async.parallelLimit(tasks, MAX_PARALLEL_LOOKUPS, (err, results) => {
-      if (err) {
-        Logger.error({ err }, 'Lookup Error');
-        cb(err);
-        return;
-      }
-
-      results.forEach(({ body, entity }) => {
-        if (
-          body === null ||
-          _isMiss(body) ||
-          _.isEmpty(body) ||
-          (body.response.results && _.isEmpty(body.response.results)) ||
-          (body.response.repositories && _.isEmpty(body.response.repositories))
-        ) {
-          lookupResults.push({
-            entity,
-            data: null
-          });
-        } else {
-          const formattedDetails = getFormattedDetails(body, options, entity);
-          lookupResults.push({
-            entity,
-            data: {
-              summary: getSummaryTags(formattedDetails),
-              details: formattedDetails
-            }
-          });
-        }
-      });
-
-      Logger.debug({ lookupResults }, 'Results');
-      cb(null, lookupResults);
+    cb(null, {
+      entity,
+      body: statusCode === 200 ? body : null
     });
   });
-}
+};
 
 const getSummaryTags = (formattedDetails) => {
   const tags = [];
@@ -254,11 +348,17 @@ const getSummaryTags = (formattedDetails) => {
     tags.push(`Has Compliance: ${formattedDetails.response.hasCompliance}`);
   }
   if (formattedDetails.response && !isNaN(formattedDetails.response.lastScan)) {
-    tags.push(`Last Scan: ${DateTime.fromSeconds(+formattedDetails.response.lastScan).toLocaleString(DateTime.DATETIME_SHORT)}`);
+    tags.push(
+      `Last Scan: ${DateTime.fromSeconds(
+        formattedDetails.response.lastScan
+      ).toLocaleString(DateTime.DATETIME_SHORT)}`
+    );
   }
   if (formattedDetails.response && formattedDetails.response.totalRecords) {
     tags.push(`${formattedDetails.response.totalRecords} records found`);
-    tags.push(`Critical Severities: ${formattedDetails.response.criticalSeverityResults.length}`);
+    tags.push(
+      `Critical Severities: ${formattedDetails.response.criticalSeverityResults.length}`
+    );
   }
   return tags;
 };
@@ -283,12 +383,16 @@ const getFormattedDetails = (body, options, entity) => ({
 });
 
 const getSeverityResults = (severityId, body) =>
-  fp.filter(fp.flow(fp.get('severity.id'), fp.eq(severityId)), fp.get('response.results', body));
+  fp.filter(
+    fp.flow(fp.get('severity.id'), fp.eq(severityId)),
+    fp.get('response.results', body)
+  );
 
 function validateStringOption(errors, options, optionName, errMessage) {
   if (
     typeof options[optionName].value !== 'string' ||
-    (typeof options[optionName].value === 'string' && options[optionName].value.length === 0)
+    (typeof options[optionName].value === 'string' &&
+      options[optionName].value.length === 0)
   ) {
     errors.push({
       key: optionName,
@@ -303,11 +407,80 @@ function validateOptions(options, callback) {
   validateStringOption(errors, options, 'url', 'You must provide a valid API URL');
   validateStringOption(errors, options, 'userName', 'You must provide a valid Username');
   validateStringOption(errors, options, 'password', 'You must provide a valid Password');
+
+  if (options.maxConcurrent.value < 1) {
+    errors = errors.concat({
+      key: 'maxConcurrent',
+      message: 'Max Concurrent Requests must be 1 or higher'
+    });
+  }
+
+  if (options.minTime.value < 1) {
+    errors = errors.concat({
+      key: 'minTime',
+      message: 'Minimum Time Between Lookups must be 1 or higher'
+    });
+  }
+
   callback(null, errors);
+}
+
+function _isInvalidEntity(entity) {
+  if (entity.value.length > MAX_ENTITY_LENGTH) {
+    return true;
+  }
+
+  if (entity.isIPv4 && IGNORED_IPS.has(entity.value)) {
+    return true;
+  }
+
+  return false;
+}
+
+function _isEntityBlocklisted(entity, options) {
+  const blocklist = options.blocklist;
+
+  Logger.trace({ blocklist: blocklist }, 'checking to see what blocklist looks like');
+
+  if (_.includes(blocklist, entity.value.toLowerCase())) {
+    return true;
+  }
+
+  if (entity.isIP && !entity.isPrivateIP) {
+    if (ipBlocklistRegex !== null) {
+      if (ipBlocklistRegex.test(entity.value)) {
+        Logger.debug({ ip: entity.value }, 'Blocked BlockListed IP Lookup');
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function onMessage(payload, options, callback) {
+  switch (payload.action) {
+    case 'retryLookup':
+      doLookup([payload.entity], options, (err, lookupResults) => {
+        if (err) {
+          Logger.error({ err }, 'Error retrying lookup');
+          callback(err);
+        } else {
+          callback(
+            null,
+            lookupResults && lookupResults[0] && lookupResults[0].data === null
+              ? { data: { summary: ['No Results Found on Retry'] } }
+              : lookupResults[0]
+          );
+        }
+      });
+      break;
+  }
 }
 
 module.exports = {
   doLookup,
+  onMessage,
   startup,
   validateOptions
 };
